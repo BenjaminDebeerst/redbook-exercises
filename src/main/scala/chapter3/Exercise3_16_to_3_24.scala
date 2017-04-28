87dad1d91681d255519e95e45ec2a7370bc4059d
Benjamin Debeerst <benjamin.debeerst@soundcloud.com>
2017-04-28 12:01:51 +0200

    Exercise 3.22

--- src/main/scala/chapter3/Exercise3_16_to_3_24.scala
+++ src/main/scala/chapter3/Exercise3_16_to_3_24.scala
@@ -5,8 +5,14 @@ object Exercise3_16_to_3_24 {
 
   def main(): Unit = {
     val list = List(1, 2, 3, 4)
-    println(add(list, 2))
-    println(toString(List(3.0, 3.123, 4)))
+
+    println(filter(list, (i: Int) => i%2==1))
+    println(filterWithFlatMap(list, (i: Int) => i%2==1))
+    println(flatMap(List(1,2,3))(e => List(e,e)))
+
+    println(vectorAdd(List(1,2,3), List(1,2,3)))
+    println(vectorAdd(List(1,2,3), List(1,2,3,0)))
+    println(vectorAdd(List(1,2,3,0), List(1,2,3)))
   }
 
   // Exercise 3.16 - 3.18
@@ -17,7 +23,36 @@ object Exercise3_16_to_3_24 {
   def add(list: List[Int], number: Int) = map(list)((i: Int) => i + number )
   def toString(list: List[Double]) = map(list)((d: Double) => d.toString )
 
-  
+  // Exercise 3.19
+  def filter[A](list: List[A], predicate: A => Boolean): List[A] = {
+    list match {
+      case Nil => Nil
+      case Cons(head, tail) if predicate(head) => filter(tail, predicate)
+      case Cons(head,tail) if !predicate(head) => Cons(head, filter(tail, predicate))
+    }
+  }
 
+  // Exercise 3.20
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] = {
+    foldRight(as, List[B]())(
+      (a, bs) =>
+        concat(f(a), bs)
+    )
+  }
+
+  // Exercise 3.21
+  def filterWithFlatMap[A](list: List[A], predicate: A => Boolean): List[A] = {
+    flatMap(list)(a => if(predicate(a)) List() else List(a))
+  }
+
+  // Exercise 3.22
+  def vectorAdd(l1: List[Int], l2: List[Int]): List[Int] = {
+    (l1, l2) match {
+      case (Nil, Nil) => Nil
+      case (Nil, Cons(h, t)) => Cons(h, t)
+      case (Cons(h, t), Nil) => Cons(h, t)
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1+h2, vectorAdd(t1, t2))
+    }
+  }
 
 }
