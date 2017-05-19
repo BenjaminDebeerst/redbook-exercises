e4c72cd6858653d2b192f6b97fa5944e0a7df095
Benjamin Debeerst <benjamin.debeerst@soundcloud.com>
2017-05-19 13:44:52 +0200

    Exercise 6.3

--- src/main/scala/chapter6/Exercises.scala
+++ src/main/scala/chapter6/Exercises.scala
@@ -22,6 +22,27 @@ object Exercises {
       rng => double(rng)
     )
 
+    println("--")
+    printN[RNG, (Int, Double)](
+      5,
+      SimpleRNG(42),
+      rng => intDouble(rng)
+    )
+
+    println("--")
+    printN[RNG, (Double, Int)](
+      20,
+      SimpleRNG(42),
+      rng => doubleInt(rng)
+    )
+
+    println("--")
+    printN[RNG, (Double, Double, Double)](
+      5,
+      SimpleRNG(42),
+      rng => double3(rng)
+    )
+
   }
 
   def printN[A, B](n: Int, seed: A, next: A => (B, A), printer: B => Unit = println(_: B)): Unit = {
@@ -30,7 +51,7 @@ object Exercises {
       case _ => {
         val (a, b) = next(seed)
         printer(a)
-        printN(n-1, b, next, printer)
+        printN(n - 1, b, next, printer)
       }
     }
   }
@@ -60,7 +81,26 @@ object Exercises {
     val (n, nextRng) = nonNegativeInt(rng)
     n match {
       case Int.MaxValue => double(nextRng) // avoid returning 1
-      case _ => ( n.toDouble / Int.MaxValue, nextRng )
+      case _ => (n.toDouble / Int.MaxValue, nextRng)
     }
+  }
+
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, nextRng) = rng.nextInt
+    val (d, _) = double(rng)
+    ((i, d), nextRng)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val (i, _) = rng.nextInt
+    val (d, nextRng) = double(rng)
+    ((d, i), nextRng)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, rng1) = double(rng)
+    val (d2, rng2) = double(rng1)
+    val (d3, rng3) = double(rng2)
+    ((d1, d2, d3), rng3)
   }
 }
