1b2e8a485ac3946694af66eb1d878ed92288cce0
Benjamin Debeerst <benjamin.debeerst@soundcloud.com>
2017-06-02 18:11:43 +0200

    Exercise 7.4 and 7.5

--- src/main/scala/chapter7/Parallelism.scala
+++ src/main/scala/chapter7/Parallelism.scala
@@ -8,16 +8,31 @@ object Parallelism {
   import Par.Par
 
   def run(): Unit = {
-    val executor = Executors.newWorkStealingPool(4)
+    val executor = Executors.newWorkStealingPool(200)
     val numbers = IndexedSeq(1,2,3,4,5,6)
-    val longSeq = List.fill(20000)(1).toIndexedSeq
+
+    def f(a: Int): Int = { println("computing"); a + 1}
+    val lazyF = Par.asyncF(f)
+    val lazyResult = lazyF(42)
+    println(lazyResult)
+    println("-----")
+    println(Par.run(executor)(lazyResult).get)
 
     measureTime(
-      () => println("Fold computation: " + numbers.foldLeft(0)(timeConsumingSum))
+      () => {
+        val result: Int = Par.run(executor)(sum(numbers, timeConsumingSum)).get
+        println("Parallel computation: " + result, result.getClass)
+      }
     )
+
+    println("======")
     measureTime(
-      () => println("Parallel computation: " + Par.run(executor)(sum(longSeq, timeConsumingSum)).get())
+      () => {
+        println((Par.parMap(List.fill(200)(1))(i => timeConsumingSum(i, 1)))(executor).get)
+      }
     )
+
+
     executor.shutdownNow()
   }
 
@@ -67,6 +82,19 @@ object Par {
 
   def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
 
+  def asyncF[A,B](f: A => B): A => Par[B] = a => lazyUnit(f(a))
+
+  def sortPar(parList: Par[List[Int]]): Par[List[Int]] = map(parList)(_.sorted)
+
+  def map[A,B](pa: Par[A])(f: A => B): Par[B] = map2(pa, unit(()))((a, _) => f(a))
+
+  def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] = {
+    val fbs: List[Par[B]] = ps.map(asyncF(f))
+    sequence(fbs)
+  }
+
+  def sequence[A](ps: List[Par[A]]): Par[List[A]] =
+    ps.foldRight(Par.unit(List[A]()))((l, pa) => map2(l, pa)((a, l) => a :: l))
 
   private case class UnitFuture[A](get: A) extends Future[A] {
     override def isDone = {println("is done?") ; true }
@@ -74,6 +102,7 @@ object Par {
     override def isCancelled = {println("cancelled?"); false}
     override def cancel(evenIfRunning: Boolean) = {println(s"cancel $evenIfRunning"); false}
   }
+
 }
 
 
